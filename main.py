769eea9c0def67894b5813c5f93db570412b755b
Emmanuel <emmanuelparis2005@gmail.com>
2024-04-28 14:47:24 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,9 @@
+usuarios = list()
+usuario = dict()
+
+while True:
+    usuario['user'] = str(input('Nome: '))
+    usuario['senha'] = str(input('Senha: '))
+    usuarios.append(usuario)
+    
+    print(usuario)
