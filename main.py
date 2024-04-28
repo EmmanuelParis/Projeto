0e70b7d0f15e2b50c944d79c266e0c95fb8caad3
Emmanuel <emmanuelparis2005@gmail.com>
2024-04-28 15:06:04 -0300

    commit teste

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-usuarios = list()
-usuario = dict()
-
-while True:
-    usuario['user'] = str(input('Nome: '))
-    usuario['senha'] = str(input('Senha: '))
-    usuarios.append(usuario)
-    
-    print(usuario)
+# Projeto :)
