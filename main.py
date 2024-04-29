709a5aa4cc64937ade38e26bd7d45c2addb863e7
Emmanuel <emmanuelparis2005@gmail.com>
2024-04-29 13:00:21 -0300

    Início dos CRUDs e Cores no terminal

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
-thirtyDaysMonth = [4,6,9,11]
 usuarios = list()
 usuario = dict()
-
+recognized = False
 while True:
     
-    print('=' * 50 )
+# MENU PRINCIPAL
+    print('\033[35m=' * 50 )
     print('CINE SERTÃO'.center(50))
-    print('-'.center(50))
+    print('<--------------->'.center(50))
     print('MENU PRINCIPAL'.center(50))
-    print('=' * 50)
+    print('=' * 50 ,'\033[36m')
 
-    print('[1] - CATÁLOGO \n[2] - LOGIN \n[3] - CADASTRO \n[0] - SAIR')
+    print('\033[34m[1] - CATÁLOGO \n[2] - LOGIN \n[3] - CADASTRO \n[0] - SAIR\033[m')
 
     option = int(input('\033[32mOpção: \033[m'))
     
+# SISTEMA DE LOGIN
     if (option == 2):
-        recognized = False
         while recognized != True:
-            username = input('Digite seu usuário: ')
-            for i in usuarios:
-                if i['user'] == username:
-                    password = input('Digite sua senha: ')
-                    if i['password'] != password:
-                        print('A senha não remete ao usuário')
+            print('\033[35m=' * 50 )
+            print('LOGIN'.center(50))
+            print('=' * 50 ,'\033[36m')
+            username = input('\033[34mDigite seu usuário: \033[m')
+            for pearson in usuarios:
+                if pearson['user'] == username:
+                    password = input('\033[34mDigite sua senha: \033[m')
+                    if pearson['password'] != password:
+                        print('\033[31mUsuário ou Senha iválidos!\033[m')
                     else:
                         recognized = True
                         print('\033[32mVocê está logado!\033[m')
             
-    if (option == 3):
+    elif (option == 3):
+        print('\033[35m=' * 50 )
+        print('CADASTRO'.center(50))
+        print('=' * 50 ,'\033[36m')
         #CONDIÇÃO DE RESET PARA O CADASTRO DO NASCIMENTO
         dataCorrect = False
+        thirtyDaysMonth = [4,6,9,11]
 
-        registerUser = str(input('Digite seu nome de usuário: '))
-        firstPassword = str(input('Digite sua senha: '))
+        registerUser = str(input('\033[34mDigite seu nome de usuário: \033[m'))
+        firstPassword = str(input('\033[34mDigite sua senha: \033[m'))
         #CHECAGEM DE SENHA
         while True:
-            secondPassword = str(input('Confirme a senha: '))
+            secondPassword = str(input('\033[34mConfirme a senha: \033[m'))
             if(firstPassword == secondPassword):
                 break
             else:
-                print('As senhas não são iguais, tente novamente.')
+                print('\033[33mAs senhas não são iguais, tente novamente.\033[m')
         #CHECAGEM DA DATA DE NASCIMENTO
         while (dataCorrect != True):
 
-            birthdate = str(input('Digite a data de nascimento [dd/mm/aaaa]: '))
+            birthdate = str(input('\033[34mDigite a data de nascimento [dd/mm/aaaa]: \033[m'))
             #CONDIÇÃO DE FILTRAGEM SEM OS PARENTESES
             if (len(birthdate) == 8 and len(birthdate) > 0):
                 birthdate = "/".join([birthdate[:2], birthdate[2:]])
@@ -54,10 +61,10 @@ while True:
                 birthdateMonth = int(birthdate[3:5])
                 #CHECAGEM SE O ANO É VALIDO
                 if((birthdateYear < 1944) or (birthdateYear> 2024)):
-                    print('Tem certeza que você está vivo?')
+                    print('\033[33mTem certeza que você está vivo?\033[m')
                 #CHECAGEM SE O MES É VALIDO
                 elif((birthdateMonth) <= 0 or (birthdateMonth > 12)):
-                    print('Mês digitado inválido!')
+                    print('\033[31mMês digitado inválido\033[m')
                 #CHECAGEM SE O DIA É VALIDO
                 elif((birthdateDay > 0) and (birthdateDay <= 31)):
                     #CHECAGEM DE ANO BISSEXTO
@@ -65,18 +72,18 @@ while True:
                         if (birthdateYear % 4 == 0 and birthdateYear % 100 != 0) or (birthdateYear % 400 == 0):
                             dataCorrect = True
                         else:
-                            print('Data informada inválida!')
+                            print('\033[31mData informada inválida!\033[m')
                     elif ((birthdateMonth in thirtyDaysMonth) and birthdateDay < 31):
                             dataCorrect = True
                     elif ((birthdateMonth not in thirtyDaysMonth) and birthdateDay <= 31):
                             if (birthdateMonth == 2 and birthdateDay > 28):
-                                print('Data informada invalida')
+                                print('\033[31mData informada inválida!\033[m')
                             else:
                                 dataCorrect = True
                     else:
-                        print('Data informada invalida')
+                        print('\033[31mData informada inválida!\033[m')
                 else:
-                    print('Data informada invalida') 
+                    print('\033[31mData informada inválida!\033[m') 
             #CONDIÇÃO DE FILTRAGEM COM OS PARENTESES    
             elif (len(birthdate) == 10 and birthdate[2] == '/' and birthdate[5] == '/'):
                 birthdateYear = int(birthdate[6:])
@@ -84,10 +91,10 @@ while True:
                 birthdateMonth = int(birthdate[3:5])
                 #CHECAGEM SE O ANO É VALIDO
                 if((birthdateYear < 1944) or (birthdateYear> 2024)):
-                    print('Tem certeza que você está vivo?')
+                    print('\033[34mTem certeza que você está vivo?\033[m')
                 #CHECAGEM SE O MES É VALIDO
                 elif((birthdateMonth) <= 0 or (birthdateMonth > 12)):
-                    print('Mês digitado inválido')
+                    print('\033[31mMês digitado inválido\033[m')
                 #CHECAGEM SE O DIA É VALIDO
                 elif((birthdateDay > 0) and (birthdateDay <= 31)):
                     #CHECAGEM DE ANO BISSEXTO
@@ -95,28 +102,28 @@ while True:
                         if (birthdateYear % 4 == 0 and birthdateYear % 100 != 0) or (birthdateYear % 400 == 0):
                             dataCorrect = True
                         else:
-                            print('Data informada invalida!')
+                            print('\033[31mData informada inválida!\033[m')
                     elif ((birthdateMonth in thirtyDaysMonth) and birthdateDay < 31):
                             dataCorrect = True
                     elif ((birthdateMonth not in thirtyDaysMonth) and birthdateDay <= 31):
                             if (birthdateMonth == 2 and birthdateDay > 28):
-                                print('Data informada invalida!')
+                                print('\033[31mData informada inválida!\033[m')
                             else:
                                 dataCorrect = True
                     else:
-                        print('Data informada inválida!')
+                        print('\033[31mData informada inválida!\033[m')
                 else:
-                    print('Data informada inválida!')            
+                    print('\033[31mData informada inválida!\033[m')          
             else:
-                print('Data informada invalida!')
+                print('\033[31mData informada inválida!\033[m')
                 
         #CHECAGEM DO EMAIL
         while (True):
-            userEmail = input('Informe seu email: ')
+            userEmail = input('\033[34mInforme seu email: \033[m')
             if(userEmail[-4:] == ".com" and (userEmail.count("@") == 1)):
                 break
             else:
-                print('Email informado errado')
+                print('\033[31mEmail informado errado\033[m')
         #CONTROLE DE ADMIN
         while(True):
             id = 0
@@ -131,10 +138,10 @@ while True:
                         print('Cadastrado como admin!')
                         break
                     else:
-                        print("Palavra chave errada!")
+                        print('\033[31mPalavra chave errada!\033[m')
                 break
             else:
-                print('Informe uma opção válida')
+                print('\033[31mInforme uma opção válida\033[m')
                 
         age = 2024 - birthdateYear
         usuario['user'] = registerUser
@@ -144,28 +151,45 @@ while True:
         usuario['id'] = id
         usuarios.append(usuario.copy())
         
-        print('Cadastro concluido com Sucesso!')
-        
-        
+        print('\033[36mCadastro concluido com Sucesso!\033[m')
+    elif option == 0:
+        print('\033[35mEncerrando o programa! Até a próxima!\033[m')
+        break
+    else:
+        print('\033[31mDigite uma opção válida!\033[m')        
 # CRUD - CLIENTE
     if recognized == True:
-        for i in usuarios:
-            if i['user'] == username:
-                if i['id'] == 0:
-                    print('Bem vindo ao sistema de clientes!')
+        for pearson in usuarios:
+            if pearson['user'] == username:
+                if pearson['id'] == 0:
+                    print('\033[35m=' * 50 )
+                    print('CINE SERTÃO'.center(50))
+                    print('<--------------->'.center(50))
+                    print('MENU CLIENTES'.center(50))
+                    print('=' * 50 ,'\033[36m')
                     while True:
-                        goBack = int(input('Deseja voltar? \n[1] - Sim \n[2] - Não \nOpção: '))
-                        if goBack == 1:
-                            recognized = False
-                            break
+                        actionMenu = int(input('\033[34mO que deseja fazer? \n[1] - Depositar Dinheiro \n[2] - Comprar Ingresso \n[3] - Avaliar Filme \n[0] - Deslogar \nOpção: \033[m'))
+
+                        if actionMenu == 0:
+                            logOff = int(input('\033[34mTem certeza que deseja deslogar? \n[1] - Sim \n[2] - Não \nOpção: \033[m'))
+                            if logOff == 1:
+                                recognized = False
+                                break
                     
 # CRUD - ADMIN
-                elif i['id'] == 1:
-                    print('Bem vindo ao sistema de admin!')
+                elif pearson['id'] == 1:
+                    print('\033[35m=' * 50 )
+                    print('CINE SERTÃO'.center(50))
+                    print('<--------------->'.center(50))
+                    print('MENU ADMIN'.center(50))
+                    print('=' * 50 ,'\033[36m')
                     while True:
-                        goBack = int(input('Deseja voltar? \n[1] - Sim \n[2] - Não \nOpção: '))
-                        if goBack == 1:
-                            recognized = False
-                            break
+                        actionMenu = int(input('\033[34mO que deseja fazer? \n[1] - Cadastrar Filme \n[2] - Buscar Filme \n[3] - Remover Filme \n[4] - Atualizar Filme \n[5] - Controle e Feedback \n[0] - Deslogar \nOpção: \033[m'))
+
+                        if actionMenu == 0:
+                            logOff = int(input('\033[34mTem certeza que deseja deslogar? \n[1] - Sim \n[2] - Não \nOpção: \033[m'))
+                            if logOff == 1:
+                                recognized = False
+                                break
     else:
         pass
